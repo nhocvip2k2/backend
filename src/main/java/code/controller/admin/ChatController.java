4d18051475b9b0c0d3ee0ae1e9b9bead772a9a6e
Doantotnghiep <doantotnghiepspring@gmail.com>
2024-11-19 11:31:06 +0700

    deploy_render

--- src/main/java/code/controller/admin/ChatController.java
+++ src/main/java/code/controller/admin/ChatController.java
@@ -3,7 +3,7 @@ package code.controller.admin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("AdminCategoryController")
+@RestController("AdminChatController")
 @RequestMapping("/api/admin/chat")
 public class ChatController {
 
